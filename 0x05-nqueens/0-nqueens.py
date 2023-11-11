1e26955bf4b4744fa1fbb6f03217275b763a8dc8
alexsameri <samerialexx@gmail.com>
2023-11-11 08:32:18 +0300

    Chess game

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,62 @@
+
+"""N Queens placement on NxN chessboard"""
+
+
+import sys
+
+
+def generate_solutions(row, column):
+    """solve a simple N x N matrix"""
+    solution = [[]]
+    for queen in range(row):
+        solution = place_queen(queen, column, solution)
+    return solution
+
+
+def place_queen(queen, column, prev_solution):
+    """Place the queen at a certain position"""
+    safe_position = []
+    for array in prev_solution:
+        for x in range(column):
+            if is_safe(queen, x, array):
+                safe_position.append(array + [x])
+    return safe_position
+
+
+def is_safe(q, x, array):
+    """check if it's safe to make a move"""
+    if x in array:
+        return False
+    else:
+        return all(abs(array[column] - x) != q - column
+                   for column in range(q))
+
+def init():
+    """Initialize the game"""
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit():
+        n = int(sys.argv[1])
+    else:
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return n
+
+
+def n_queens():
+    """The main entry point"""
+    n = init()
+    solutions = generate_solutions(n, n)
+    for array in solutions:
+        clean = []
+        for q, x in enumerate(array):
+            clean.append([q, x])
+        print(clean)
+
+
+if __name__ == '__main__':
+    n_queens()
---
+++ 0x05-nqueens/README.md
@@ -0,0 +1,1 @@
+# chess sacrifice the queen
